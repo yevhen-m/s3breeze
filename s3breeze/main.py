42b74b04749d78496c3ca0d2b66eea42ba13291b
Maksym Feschenko <maxfeschenko@gmail.com>
2019-03-05 16:52:41 +0200

    skip decoding if json is already valid

--- s3breeze/main.py
+++ s3breeze/main.py
@@ -22,8 +22,8 @@ logger = logging.getLogger(__name__)
 def xml_formatter(text):
     try:
         text = json.loads(text)
-        if type(text) is dict:
-            return None
+        if not isinstance(text, str):
+            return
     except json.JSONDecodeError:
         pass
     try:
